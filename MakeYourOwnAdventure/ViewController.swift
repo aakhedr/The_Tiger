ff18b7511c8a9bb53b1c7ccaeb1a47b6a4f22549
Ahmed Khedr <ahmed.a.khedr@outlook.com>
2015-04-10 03:26:01 +0200

    Add right bar button programattically

--- MakeYourOwnAdventure/ViewController.swift
+++ MakeYourOwnAdventure/ViewController.swift
@@ -13,6 +13,8 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        self.navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Start Over", style: UIBarButtonItemStyle.Plain, target: self, action: "startOver")
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +22,10 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    func startOver() {
+        if let navigationController = self.navigationController? {
+            navigationController.popToRootViewControllerAnimated(true)
+        }
+    }
 }
 
